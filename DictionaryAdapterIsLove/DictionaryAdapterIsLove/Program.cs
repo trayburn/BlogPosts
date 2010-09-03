02786b7fc10d737887f5336011f2173b9e717e31
Tim Rayburn <tim@timrayburn.net>
2010-09-03 12:01:02 -0500

    Dictionary Adapter Is Love (Part 1)

---
+++ DictionaryAdapterIsLove/DictionaryAdapterIsLove/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Components.DictionaryAdapter;
+
+namespace DictionaryAdapterIsLove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, object>()
+                {
+                    { "UserId", 1234567 },
+                    { "UserFirstName", "Tim" },
+                    { "UserLastName", "Rayburn" }
+                };
+
+            IUserData data = new DictionaryAdapterFactory().GetAdapter<IUserData>(dict);
+
+            Console.WriteLine(data.FirstName);
+            Console.ReadLine();
+        }
+
+        //public static string UglyWayToGetUserFirstName(Dictionary<string, object> dict)
+        //{
+        //    string userFirstName = "UserFirstName";
+        //    if (dict.ContainsKey(userFirstName)) return dict[userFirstName] as string;
+        //    return null;
+        //}
+
+        //public static string CleanWayToGetUserFirstName(IUserData data)
+        //{
+        //    return data.UserFirstName;
+        //}
+    }
+
+    public interface IUserData
+    {
+        [Key("UserId")]
+        int Id { get; set; }
+
+        [Key("UserFirstName")]
+        string FirstName { get; set; }
+
+        [Key("UserLastName")]
+        string LastName { get; set; }
+    }
+
+}
